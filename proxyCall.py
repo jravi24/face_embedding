b571a46f0b47061f718406e9fa363bac3d3d572b
jravi24 <43846003+jravi24@users.noreply.github.com>
2025-05-09 10:03:01 +0530

    Update proxyCall.py

--- proxyCall.py
+++ proxyCall.py
@@ -19,7 +19,7 @@ async def proxy(request: APIRequest):
             response = await client.request(
                 method=request.method.upper(),
                 #url=request.url,
-                url="https://05c7-34-145-184-162.ngrok-free.app/generateEmbedding",
+                url="https://ac55-35-185-235-56.ngrok-free.app/generateEmbedding",
                 headers=request.headers,
                 params=request.params,
                 json=request.body  # Automatically sets content-type to application/json
